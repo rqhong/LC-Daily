17aef23e239eb01f7929047432d567cc90535773
洪睿謙 <143863497+rqhong@users.noreply.github.com>
2025-01-04 01:43:16 +0800

    Time: 62 ms (68.93%) | Memory: 21.3 MB (64.08%) - LeetSync

---
+++ 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def waysToSplitArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        count = 0
+        total_sum = sum(nums)
+        left_sum = 0
+
+        for i in range(len(nums)-1):
+            left_sum += nums[i]
+            right_sum = total_sum - left_sum
+            if left_sum >= right_sum:
+                count += 1
+
+        return count
